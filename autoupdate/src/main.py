9f4b0f27196cf90fb66a35ca01c237d15e6fa22e
CUPZYY <richbanana1234@gmail.com>
2021-05-23 17:02:04 +0200

    removed all prints cuz thats what broke the program

--- autoupdate/src/main.py
+++ autoupdate/src/main.py
@@ -4,7 +4,6 @@ import uuid
 import zipfile
 import requests
 import shutil
-from colorama import Fore, init
 from clint.textui import progress
 import threading
 from gui import *
@@ -18,30 +17,25 @@ time.sleep(2)
 
 
 def doneError():
-    print(Fore.RESET)
     gui.doneF()
     sys.exit()
 
 
 def doneEZ():
-    print(Fore.RESET)
     gui.done()
     sys.exit()
 
 
-init()
-
 if len(sys.argv) != 3:
-    print(Fore.RED + "Invalid argument size")
-    print(Fore.RED + "Syntax: installer.py [old-mod] [new-mod-url]")
 
     doneError()
 
+
 oldmodfile = sys.argv[1]
 url = sys.argv[2]
-tempfile = "bh-" + str(uuid.uuid4())
 
-print("Downloading latest build..")
+tempfile = f"bh-{str(uuid.uuid4())}"
+
 
 r = requests.get(url, stream=True)
 with open(tempfile, "wb") as f:
@@ -58,7 +52,6 @@ with open(tempfile, "wb") as f:
             f.flush()
 
 if url.endswith(".zip"):
-    print("Extracting jar..")
     with zipfile.ZipFile(tempfile, 'r') as zp:
         files = zipfile.ZipFile.infolist(zp)
         if len(files) > 0:
@@ -72,7 +65,6 @@ if url.endswith(".zip"):
     os.remove(tempfile)
     tempfile = unzippedfile
 
-print("Replacing jar..")
 
 firstLine = False
 while True:
@@ -81,17 +73,11 @@ while True:
     try:
         os.remove(oldmodfile)
 
-        if firstLine:
-            print();
-
-        break;
+        break
     except Exception as e:
         if not os.path.exists(oldmodfile):
-            print(Fore.YELLOW + "- File already deleted??")
-            break;
+            break
 
-        if firstLine:
-            print(".", end="")
         else:
             gui.minecraft()
             firstLine = True
@@ -101,9 +87,7 @@ while True:
 try:
     shutil.move(tempfile, os.path.dirname(oldmodfile) + rf"\{os.path.basename(url)}")
 except Exception as e:
-    print(Fore.RED + "Unable to move jar to mod directory, Aborting!")
     doneError()
 
-print(Fore.GREEN + "\nInstalled Successfully!")
 
 doneEZ()
