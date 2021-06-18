23bb8e7935e83fdd3c2205c78cf78ff37a709eeb
CUPZYY <69486653+CUPZYY@users.noreply.github.com>
2021-06-18 12:38:23 +0200

    removed a bunch of fortnitage

--- autoupdate/src/gui.py
+++ autoupdate/src/gui.py
@@ -6,19 +6,12 @@ class guiClass():
     def gui(self):
         self.root = Tk()
 
-        # Get the Tk instance
-
-        # Set the layout dimension
         self.root.geometry("500x300")
 
-        # Title the window
         self.root.title("BH-CupEdition Update")
 
         label = Label(text="Updating BleachHack CupEdition", font=("Consolas", 20))
-
-        # Prepare the type of Progress bar needed.
-        # Look out for determinate mode and pick the suitable one
-        # Other formats of display can be suitably explored
+        
         processing_bar = ttk.Progressbar(self.root, orient='horizontal', mode='indeterminate', length=380, maximum=40)
 
         self.successLabel = Label(self.root, text="", font=("Consolas", 20))
@@ -27,8 +20,6 @@ class guiClass():
         label.place(relx=0.5, rely=0.3, anchor=CENTER)
         processing_bar.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-        # Start the bar.
-        # use processing_bar.stop() to stop it
         processing_bar.start(30)
 
         self.root.mainloop()
